328e65597087c6fde706f2014086eed010e9e0bc
唐云 <tangyun1992@qq.com>
2021-05-15 21:50:25 +0800

    完成路由导航守卫

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import ColumnDetail from '@/views/ColumnDetail.vue'
 import CreatePost from '@/views/CreatePost.vue'
+import store from './store'
 
 const routerHistory = createWebHistory()
 const router = createRouter({
@@ -16,7 +17,10 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {
+        redirectAlreadyLogin: true
+      }
     },
     {
       path: '/column/:id',
@@ -26,9 +30,22 @@ const router = createRouter({
     {
       path: '/create',
       name: 'create',
-      component: CreatePost
+      component: CreatePost,
+      meta: {
+        requiredLogin: true
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiredLogin && !store.state.user.isLogin) {
+    next('/login')
+  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
